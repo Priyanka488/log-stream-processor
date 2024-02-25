0bfd87a22aa03dd8aff5a66e0d17a40da6ceaf2d
Priyanka488 <priyanka89547@gmail.com>
2024-02-25 18:36:33 +0530

    tcp_3

--- internal/tcp/tcp.go
+++ internal/tcp/tcp.go
@@ -12,6 +12,7 @@ func Init(wg *sync.WaitGroup) {
 
 	defer wg.Done()
 
+	var wg_tcp sync.WaitGroup
 	// 1. create a listener
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", config.TCP_PORT))
 	if err != nil {
@@ -27,6 +28,16 @@ func Init(wg *sync.WaitGroup) {
 			fmt.Println("Error accepting:", err.Error())
 			return
 		}
+		wg_tcp.Add(1)
+		go handleRequest(conn, &wg_tcp)
+	}
+
+}
+
+// 4. keep listening on the same connection
+func handleRequest(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
 		chunk := make([]byte, config.TCP_MESSAGE_SIZE)
 		readBytes, err := conn.Read(chunk)
 		if err != nil {
